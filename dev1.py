e2e1c482d96e47006ec39412a163720e91098062
Materusse <130571994+Materusse@users.noreply.github.com>
2023-05-02 17:14:49 -0300

    preenche frota

--- dev1.py
+++ dev1.py
@@ -30,11 +30,20 @@ def define_posicoes(linha, coluna, orientacao, tamanho):
     
 
 
+def define_posicoes(linha, coluna, orientacao, tamanho):
+    posicao = []
+    if orientacao == 'horizontal':
+        for i in range(tamanho):
+            posicao.append([linha, coluna + i])
+    elif orientacao == 'vertical':
+        for i in range(tamanho):
+            posicao.append([linha + i, coluna])
+    return posicao
+def preenche_frota(frota, nome_navio, linha, coluna, orientacao, tamanho):
+    posicoes_navio = define_posicoes(linha, coluna, orientacao, tamanho)
+    if nome_navio in frota:
+        frota[nome_navio].append(posicoes_navio)
+    else:
+        frota[nome_navio] = [posicoes_navio]
+    return frota
 
-
-
-
-print(define_posicoes(linha = 2,
-coluna = 4,
-orientacao = "vertical",
-tamanho = 3))
